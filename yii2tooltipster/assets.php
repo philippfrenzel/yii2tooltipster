7c188c40ed5d0677e5b647f7b580b35a6158f164
Philipp Frenzel <philipp@frenzel.net>
2013-07-09 16:49:26 +0200

    initial upload

---
+++ yii2tooltipster/assets.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+	'yii2tooltipster/core' => array(
+		'sourcePath' => __DIR__ . '/assets',
+		'js' => array(
+			'js/tooltipster.min.js'
+		),
+		'css' => array(
+			'css/reset.css',
+			'css/style.css',
+			'css/tooltipster.css',
+			'css/themes/light.css'
+		),
+		'depends'=>array(
+			'yii/jquery'
+		)
+	)
+);
---
+++ yii2tooltipster/yii2tooltipster.php
@@ -0,0 +1,74 @@
+<?php
+
+ /**
+ * This class is merely used to publish a TOC based upon the headings within a defined container
+ * @copyright Frenzel GmbH - www.frenzel.net
+ * @link http://www.frenzel.net
+ * @author Philipp Frenzel <philipp@frenzel.net>
+ *
+ */
+
+namespace yii2tooltipster;
+
+use Yii;
+use yii\base\Model;
+use yii\base\View;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\base\Widget as Widget;
+
+class yii2tooltipster extends Widget
+{
+
+    /**
+    * @var array the HTML attributes (name-value pairs) for the field container tag.
+    * The values will be HTML-encoded using [[Html::encode()]].
+    * If a value is null, the corresponding attribute will not be rendered.
+    */
+    public $options = array(
+        'class' => '.tooltip',
+    );
+
+    /**
+     * Initializes the widget.
+     * If you override this method, make sure you call the parent implementation first.
+     */
+    public function init()
+    {
+        //checks for the element id
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        }
+
+        parent::init();
+    }
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        $this->registerPlugin();
+    }
+
+    /**
+    * Registers a specific dhtmlx widget and the related events
+    * @param string $name the name of the dhtmlx plugin
+    */
+    protected function registerPlugin()
+    {
+        $id = $this->options['id'];
+
+        //get the displayed view and register the needed assets
+        $view = $this->getView();
+        $view->registerAssetBundle("yii2tooltipster/core");
+
+        $js = array();
+        
+        $className = $this->options['class'];
+        $js[] = "$('$className').tooltipster();";
+        
+        $view->registerJs(implode("\n", $js),View::POS_READY);
+    }
+
+}
