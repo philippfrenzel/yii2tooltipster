b857820ed97fd223f33910d9bddc14638a02a3c7
Philipp Frenzel <philipp@frenzel.net>
2014-03-18 20:15:51 +0100

    cleaning up case sensitivity

---
+++ yii2tooltipster/yii2tooltipsterAsset.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @link http://www.frenzel.net/
+ * @author Philipp Frenzel <philipp@frenzel.net> 
+ */
+
+namespace philippfrenzel\yii2tooltipster;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class yii2tooltipsterAsset extends AssetBundle
+{
+    public $sourcePath = '@philippfrenzel/yii2tooltipster/assets';
+    
+    public $css = array(
+        'css/tooltipster.css',
+        'css/themes/tooltipster-light.css'
+    );
+    
+    public $js = array(
+        'js/jquery.tooltipster.min.js'
+    );
+    
+    public $depends = array(
+        'yii\web\JqueryAsset',
+    );
+}
